db6f48bce27fd5f70e216e47167f78ee60619a74
yiguo-17 <yiguo.qu@codeimmersives.com>
2020-07-28 15:55:13 -0400

    edit method based on test file.

--- main.js
+++ main.js
@@ -1,7 +1,39 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const newPerson = function(firstName,lastName,age,marriage){
+const personal = {};
+personal.firstName = firstName;
+if(!firstName){personal.firstName = 'Anonymous';}
+personal.lastName = lastName;
+if(!lastName){personal.lastName = 'Person';}
+personal.age = age;
+personal.married = marriage; 
+if(!marriage){personal.married = false;}
+personal.goingOn = function(){
+  return this.age+1
+}
+personal.ageUp = function(){
+  this.age++;
+}
+personal.getFullName = function(){
+  return `${this.firstName} ${this.lastName}`
+}
+personal.marry= function(partner){
+  personal.married = true;
+  partner.married = true;
+  personal.spouseName = partner.getFullName();
+  partner.spouseName = personal.getFullName();
+}
 
+personal.divorce=function(ex){
+  personal.married = false;
+  ex.married = false;
+  delete personal.spouseName;
+  delete ex.spouseName;
+}
+return personal
+}
 
 
 /*********************************
